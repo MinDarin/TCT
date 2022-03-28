8dbbf3d51b2f1c2078f0866e56c2164f52dae8e5
김민석 <mementohora96@gmail.com>
2022-03-28 22:14:44 +0900

    Initial commit

---
+++ src/main/java/Toy/TCT/TctApplication.java
@@ -0,0 +1,13 @@
+package Toy.TCT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TctApplication.class, args);
+	}
+
+}
